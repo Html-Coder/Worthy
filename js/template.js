24b43535ac2fac080bf39ef4213641162c1d8c2c
T <thierry@yuit.net>
2017-11-18 18:45:35 +0100

    Fix error generated by update to jquery v1.12.4

--- js/template.js
+++ js/template.js
@@ -47,7 +47,7 @@
 		//Smooth Scroll
 		//-----------------------------------------------
 		if ($(".smooth-scroll").length>0) {
-			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+			$('.smooth-scroll a[href*="#"]:not([href="#"]), a[href*="#"]:not([href="#"]).smooth-scroll').click(function() {
 				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 					var target = $(this.hash);
 					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
